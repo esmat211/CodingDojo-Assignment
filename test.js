c04015694a96ca5eeb7f3a6b4f40c166f5f1ae05
Esmatullah Mohammadi <EsmatMohammadi@10003.local>
2016-12-30 22:10:53 -0800

    I want to add my assignments to github

---
+++ test.js
@@ -0,0 +1,27 @@
+// var stuff = "Hello World";
+//
+// // console.log(stuff)
+
+// var No = -1;
+// console.log(No)
+
+// var stuff = false;
+// console.log(stuff);
+
+// var stuff = [3, "hello", true, [1,2,3], {"key": "value"}, null];
+// console.log(stuff.length);
+// console.log(other.length);
+// console.log(stuff[2]);
+
+
+//Object //
+//
+// var stuff = {"name": "Jonathan", "age": 99, "coder": true};
+// console.log(stuff);
+// console.log(stuff["name"])
+// console.log(stuff.length);
+
+for (var i = 0; i < 10; i+1) {
+if(i%2!=0){
+  console.log(i);}
+}
